50a985bd5a61c2aa3ff2d01b0250b14043e8ddf3
Vladislav Dubiaga <robertron888@gmail.com>
2021-11-29 01:48:03 +0300

    bug fixes in task 1

--- Task1/multi-project/core/src/main/java/com/epam/Utils.java
+++ Task1/multi-project/core/src/main/java/com/epam/Utils.java
@@ -2,9 +2,14 @@ package com.epam;
 
 import com.epam.util.StringUtils;
 
-public class Utils
-{
-    public static boolean isAllPositive(String... str){
+public class Utils {
+    private Utils(){
+
+    }
+    public static boolean isAllPositive(String... str) {
+        if (str == null) {
+            return false;
+        }
         for (String s : str) {
             if (!StringUtils.isPositiveNumber(s)) {
                 return false;
--- Task1/utils-1.3.5/src/test/java/com/epam/util/StringUtilsTest.java
+++ Task1/utils-1.3.5/src/test/java/com/epam/util/StringUtilsTest.java
@@ -1,24 +1,32 @@
 package com.epam.util;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
-public class StringUtilsTest
-{
+public class StringUtilsTest {
     @Test
-    public void isPositiveNumberTest1()
-    {
+    public void isPositiveNumberTest1() {
         assertFalse(StringUtils.isPositiveNumber("0"));
     }
 
     @Test
-    public void isPositiveNumberTest2()
-    {
+    public void isPositiveNumberTest2() {
         assertFalse(StringUtils.isPositiveNumber("-1"));
     }
+
     @Test
-    public void isPositiveNumberTest3()
-    {
+    public void isPositiveNumberTest3() {
         assertTrue(StringUtils.isPositiveNumber("1"));
+    }
+
+    @Test
+    public void isPositiveNumberTest4() {
+        assertTrue(StringUtils.isPositiveNumber("1.0"));
+    }
+
+    @Test
+    public void isPositiveNumberTest5() {
+        assertFalse(StringUtils.isPositiveNumber(null));
     }
 }
